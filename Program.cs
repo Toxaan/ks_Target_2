628aa3729a17f776ff3e6abbbed3dc26e5b77cb0
anton_kuchkov <kuanton@mail.ru>
2020-07-07 20:58:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ks_Target_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(" ");
+
+            int n = int.Parse(str[0]);
+            int m = int.Parse(str[1]);
+
+            Console.WriteLine(F(m - 1) / (F(n - 1) * F(m - n)));
+
+        }
+
+        static int F(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * F(x - 1);
+            }
+        }
+    }
+}
